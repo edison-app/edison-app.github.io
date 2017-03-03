0642f6419d5c0d979b3d4ff8ec722a55cd574c9e
adminAmbitGroup <dthor@theambitgroup.com>
2017-03-03 17:54:36 -0500

    Update d3controllers.js

--- app/controllers/d3controllers.js
+++ app/controllers/d3controllers.js
@@ -6,5 +6,5 @@ features.controller('bubbleCtrl', ['$scope', function($scope) {
    { "x_axis": 480, "y_axis": 180, "radius": 60, "color" : "blue","image": "map","url":"ttps://eddev.service-now.com/spa/maps.do" },
    { "x_axis": 300, "y_axis": 500, "radius": 60, "color" : "#fff","image": "tree","url":"https://www.google.com" },
    { "x_axis": 120, "y_axis": 380, "radius": 60, "color" : "#fff","image": "globe","url":"https://www.google.com" },
-   { "x_axis": 480, "y_axis": 380, "radius": 60, "color" : "#fff","image":"scatter","url":"https://www.google.com" }]
+   { "x_axis": 480, "y_axis": 380, "radius": 60, "color" : "#fff","image":"scatter","url":"https://edison-app.github.io/projects/dashboard/" }]
 }]);
