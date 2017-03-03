0d037694e7131c91578d8aabeaf4bfc832c6d4cf
adminAmbitGroup <dthor@theambitgroup.com>
2017-03-03 12:49:43 -0500

    Add files via upload

---
+++ projects/dashboard/app/controllers/tblControllers.js
@@ -0,0 +1,41 @@
+features.controller("tableController", ['$scope', function ($scope) {
+    $scope.tblChartPrograms = [
+        {
+            "duns": "038749560",
+            "name": "SOUTHERN CALIFORNIA INDIAN CENTER INC",
+            "state": "CA"
+        },
+        {
+            "duns": "073364358",
+            "name": "QUECHAN INDIAN TRIBE",
+            "state": ""
+        },
+        {
+            "duns": "074647165",
+            "name": "HOOPA VALLEY TRIBAL COUNCIL",
+            "state": "CA"
+        },
+        {
+            "duns": "087581153",
+            "name": "SAN DIEGO COUNTY OFFICE OF EDUCATION",
+            "state": "CA"
+        },
+        {
+            "duns": "138603787",
+            "name": "SANTA YNEZ BAND MISSION INDIANS",
+            "state": "CA"
+        },
+        {
+            "duns": "145307930",
+            "name": "KARUK TRIBE",
+            "state": "CA"
+        },
+        {
+            "duns": "883847626",
+            "name": "PINOLEVILLE POMO NATION",
+            "state": "CA"
+        }
+    ]
+
+}]);
+
