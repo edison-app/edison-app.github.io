a9b516186600ca85d5af5fc9f0aab59c02c157f1
dthor1981 <dthor1981@gmail.com>
2017-04-19 16:31:20 -0400

    new

--- projects/Oii/app/controllers/Controllers.js
+++ projects/Oii/app/controllers/Controllers.js
@@ -1,24 +1,27 @@
-app.controller('prHttpCtrl',['$scope', function($scope){
+app.controller('prHttpCtrl', ['$scope', function ($scope) {
     $scope.prAwardNum = "PR Award Number";
 }])
 
-
-app.controller("SimpleDemoController",['$scope', function($scope) {
+app.controller('dragDropCtrl', ['$scope', function ($scope) {
 
     $scope.models = {
         selected: null,
-        lists: {"Select": [], "Drop": []}
+        lists: { "Select": [], "Drop": [] }
     };
 
     // Generate initial model
     for (var i = 1; i <= 3; ++i) {
-        $scope.models.lists.Select.push({label: "Item A" + i});
-        $scope.models.lists.Drop.push({label: "Item B" + i});
+        $scope.models.lists.Select.push({ label: "Item A" + i });
+        $scope.models.lists.Drop.push({ label: "Item B" + i });
     }
 
     // Model to JSON for demo purpose
-    $scope.$watch('models', function(model) {
+    $scope.$watch('models', function (model) {
         $scope.modelAsJson = angular.toJson(model, true);
     }, true);
 
 }]);
+
+app.controller('yrDropCtrl', ['$scope', function ($scope) {
+    $scope.years = [2016, 2015, 2014, 2013, 2012, 2011, 2010];
+}])
--- projects/Oii/app/directives/Directives.js
+++ projects/Oii/app/directives/Directives.js
@@ -1,16 +1,32 @@
 
 app.directive('prHref', ['$location', '$window', function ($location, $window) {
-        return{
-            restrict: 'A',
-            link: function (scope, element, attr) {
-                element.attr('style', 'cursor:pointer');
-                element.on('click', function(){
-                   // var prInput = angular.element(document.getElementById('pr-input')).value;
-                    var host = $location.host();
-                    var landingUrl = "pr_results.html?prAwardNum="+ attr.prHref;
-                    $window.location.href = landingUrl;
-                    // $window.location.search = attr.programHref;
-                });
-                }
+    return {
+        restrict: 'A',
+        link: function (scope, element, attr) {
+            element.attr('style', 'cursor:pointer');
+            element.on('click', function () {
+                // var prInput = angular.element(document.getElementById('pr-input')).value;
+                var host = $location.host();
+                var landingUrl = "pr_results.html?prAwardNum=" + attr.prHref;
+                $window.location.href = landingUrl;
+                // $window.location.search = attr.programHref;
+            });
         }
-        }]);
+    }
+}]);
+
+app.directive('yrDropHref', ['$location', '$window', function ($location, $window) {
+    return {
+        restrict: 'A',
+        link: function (scope, element, attr) {
+            element.attr('style', 'cursor:pointer');
+            element.on('click', function (event) {
+                        var host = $location.host();
+                        var path = $location.path();
+                        var search = $location.search();
+                        var landingUrl = host + path + search + attr.yrDropHref;
+                        $window.location.href = landingUrl;
+            });
+        }
+    }
+}]);
