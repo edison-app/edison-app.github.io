8de5242b33f80e4e5eafa3d9993f746958a1a377
David Thor <dthor1981@gmail.com>
2017-04-29 00:51:25 -0400

    latest

--- projects/Oii/app/controllers/Controllers.js
+++ projects/Oii/app/controllers/Controllers.js
@@ -26,7 +26,54 @@ app.controller('switchBtnCtrl', ['$scope', '$window', '$location', function($sco
     }
 
   }
-}])
+}]);
+
+
+app.controller('paginationCtrl', ['$scope', '$filter', function ($scope, $filter) {
+  {
+      $scope.currentPage = 0;
+      $scope.pageSize = 10;
+      $scope.data = [];
+      $scope.codes = '';
+
+      $scope.getData = function () {
+        // needed for the pagination calc
+        // https://docs.angularjs.org/api/ng/filter/filter
+        return $filter('filter')($scope.data, $scope.codes)
+       /*
+         // manual filter
+         // if u used this, remove the filter from html, remove above line and replace data with getData()
+
+          var arr = [];
+          if($scope.q == '') {
+              arr = $scope.data;
+          } else {
+              for(var ea in $scope.data) {
+                  if($scope.data[ea].indexOf($scope.q) > -1) {
+                      arr.push( $scope.data[ea] );
+                  }
+              }
+          }
+          return arr;
+         */
+      }
+
+      $scope.numberOfPages=function(){
+          return Math.ceil($scope.getData().length/$scope.pageSize);
+      }
+
+      for (var i=0; i<65; i++) {
+          $scope.data.push("Item "+i);
+      }
+}
+}]);
+
+app.filter('startFrom', function() {
+    return function(input, start) {
+        start = +start; //parse to int
+        return input.slice(start);
+    }
+});
 
 app.controller('dragDropCtrl', ['$scope', '$window', '$localStorage', '$location', function($scope, $localStorage, $window, $location) {
 
@@ -104,7 +151,7 @@ app.controller('dragDropCtrl', ['$scope', '$window', '$localStorage', '$location
       }
   });*/
 
-  for (var i = 1; i <= 10; ++i) {
+  for (var i = 1; i <= 20; ++i) {
     $scope.models[0].items.push({
       label: "Item Code: " + i
     });
